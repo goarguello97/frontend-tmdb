381e08f89ed6a5eed548a994ee04f33bedeb67f5
Gonzalo Nicolás Argüello <arguellogonzalo97@gmail.com>
2023-04-28 17:45:51 -0300

    Cambios en titulo de html, eliminación de archivos

--- src/components/RegisterForm/RegisterForm.tsx
+++ src/components/RegisterForm/RegisterForm.tsx
@@ -6,15 +6,15 @@ const RegisterForm = () => {
     <Form className="form-container">
       <h1>Registro</h1>
       <hr />
-      <Form.Group className="mb-3" controlId="formBasicEmail">
+      <Form.Group className="mb-3" controlId="name">
         <Form.Label>Nombre</Form.Label>
         <Form.Control type="text" placeholder="Nombre" name="name" />
       </Form.Group>
-      <Form.Group className="mb-3" controlId="formBasicEmail">
+      <Form.Group className="mb-3" controlId="lastname">
         <Form.Label>Apellido</Form.Label>
         <Form.Control type="text" placeholder="Apellido" name="lastname" />
       </Form.Group>
-      <Form.Group className="mb-3" controlId="formBasicEmail">
+      <Form.Group className="mb-3" controlId="email">
         <Form.Label>Correo electrónico</Form.Label>
         <Form.Control
           type="email"
@@ -23,7 +23,7 @@ const RegisterForm = () => {
         />
       </Form.Group>
 
-      <Form.Group className="mb-3" controlId="formBasicPassword">
+      <Form.Group className="mb-3" controlId="password">
         <Form.Label>Contraseña</Form.Label>
         <Form.Control
           type="password"
