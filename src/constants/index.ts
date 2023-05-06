7b92949465bd84aaca113509c549ac1dbff475a9
Gonzalo Nicolás Argüello <arguellogonzalo97@gmail.com>
2023-05-06 11:03:06 -0300

    Configuración redux

---
+++ src/constants/index.ts
@@ -0,0 +1,6 @@
+export const REGISTER_INITIAL_VALUES = {
+  name: "",
+  lastname: "",
+  email: "",
+  password: "",
+};
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,35 +1,61 @@
 import Carousel from "react-bootstrap/Carousel";
 import Card from "../commons/Card/Card";
 import useMediaQuery from "../hooks/useMediaQuery";
+import { useEffect } from "react";
+import { getPopular } from "../features/movies/moviesSlice";
+import { useAppDispatch, useAppSelector } from "../hooks/useTypedSelector";
+import { Movie, TvSeries } from "../interfaces/movies.interface";
 
 const Home = () => {
   const { width } = useMediaQuery();
+  const dispatch = useAppDispatch();
+  const { movies, tvSeries} = useAppSelector((state) => state.movies);
+
+  useEffect(() => {
+    dispatch(getPopular());
+  }, []);
+
   return (
     <div className="container-fluid home">
       {width > 768 ? (
-        <div className="movies-container">
-          <Card />
-          <Card />
-          <Card />
-          <Card />
-          <Card />
-          <Card />
-          <Card />
-          <Card />
-          <Card />
-          <Card />
-          <Card />
-          <Card />
-          <Card />
-        </div>
+        <>
+          <h1>Peliculas recomendadas</h1>
+          <div className="movies-container">
+            {movies?.map((movie: Movie, i) => (
+              <Card
+                key={i}
+                image={movie.poster_path}
+                title={movie.title}
+                id={movie.id}
+                typeFilm={"movie"}
+              />
+            ))}
+          </div>
+          <h1>Series recomendadas</h1>
+          <div className="movies-container">
+            {tvSeries?.map((movie: TvSeries, i) => (
+              <Card
+                key={i}
+                image={movie.poster_path}
+                title={movie.name}
+                id={movie.id}
+                typeFilm={"tv"}
+              />
+            ))}
+          </div>
+        </>
       ) : (
         <Carousel indicators={false}>
-          <Carousel.Item>
-            <Card />
-          </Carousel.Item>
-          <Carousel.Item>
-            <Card />
-          </Carousel.Item>
+          {movies?.map((movie: Movie, i) => (
+            <Carousel.Item key={i}>
+              <Card
+                image={movie.poster_path}
+                title={movie.title}
+                id={movie.id}
+                typeFilm={"movie"}
+              />
+            </Carousel.Item>
+          ))}
         </Carousel>
       )}
     </div>
